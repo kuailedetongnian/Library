69772599f556a2c3ea11858af17420db91308aef
kuailedetongnian <94746183+kuailedetongnian@users.noreply.github.com>
2022-05-27 22:46:06 +0800

    Add files via upload
    做了一个东西 Continuity_t

---
+++ C/Include/Continuity.h
@@ -0,0 +1,65 @@
+//
+// Continuity.h
+//     Copyright (c) kuailedetongnian all rights reserved.
+//
+
+//
+// /C/Include/Continuity.h
+//
+
+#ifndef _CONTINUITY_H_
+#define _CONTINUITY_H_
+
+#include "./types.h"
+#include <malloc.h>
+
+void* (*Continuity_malloc)(const size_t) = malloc;
+
+typedef struct Continuity_t {
+    void*  data;
+    size_t size;
+} Continuity_t;
+
+Continuity_t*
+Continuity_init(
+    Continuity_t *const conty
+);
+
+Continuity_t*
+Continuity_resize(
+    Continuity_t *const conty,
+    const size_t        size,
+    const bool          copy
+);
+
+Continuity_t*
+Continuity_nResize(
+    Continuity_t *const conty,
+    const size_t        size,
+    const size_t        desBeginIndex,
+    const size_t        srcBeginIndex,
+    const size_t        copySize
+);
+
+Continuity_t*
+Continuity_free(
+    Continuity_t *const conty
+);
+
+Continuity_t*
+Continuity_nCopy(
+    Continuity_t *const conty,
+    const void *const   data,
+    const size_t        size,
+    const size_t        index
+);
+
+Continuity_t*
+Continuity_copy(
+    Continuity_t *const       desConty,
+    const Continuity_t *const srcConty
+);
+
+#include "src/Continuity.c"
+
+#endif /* _CONTINUITY_H_ */
