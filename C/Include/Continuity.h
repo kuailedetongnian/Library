c317b80b0b1b09faf893ba72bcb604ae06f88ffb
kuailedetongnian <94746183+kuailedetongnian@users.noreply.github.com>
2022-05-29 11:12:54 +0800

    Update Continuity.h

--- C/Include/Continuity.h
+++ C/Include/Continuity.h
@@ -1,36 +1,64 @@
 //
-// Continuity.h
+// Continuity.c
 //     Copyright (c) kuailedetongnian all rights reserved.
 //
 
 //
-// /C/Include/Continuity.h
+// /C/Include/src/Continuity.c
 //
 
-#ifndef _CONTINUITY_H_
-#define _CONTINUITY_H_
+#ifndef _CONTINUITY_C_
+#define _CONTINUITY_C_
 
-#include "./types.h"
-#include <malloc.h>
-
-void* (*Continuity_malloc)(const size_t) = malloc;
-
-typedef struct Continuity_t {
-    void*  data;
-    size_t size;
-} Continuity_t;
+#include "../Continuity.h"
+#include "../types.h"
+#include <string.h>
 
 Continuity_t*
 Continuity_init(
-    Continuity_t *const conty
-);
+    Continuity_t *const conty)
+{
+    if(conty == NULL) {
+        return NULL;
+    }
+    conty->data = NULL;
+    conty->size = 0;
+    return conty;
+}
 
 Continuity_t*
 Continuity_resize(
     Continuity_t *const conty,
     const size_t        size,
-    const bool          copy
-);
+    const bool          copy)
+{
+    if(conty == NULL) {
+        return NULL;
+    }
+    if(size == 0) {
+        Continuity_free(conty);
+        return conty;
+    }
+    if(conty->data == NULL) {
+        conty->data = Continuity_malloc(size);
+        if(conty->data == NULL) {
+            return NULL;
+        }
+        conty->size = size;
+        return conty;
+    }
+    void *const oldContyData = conty->data;
+    conty->data = Continuity_malloc(size);
+    if(conty->data == NULL) {
+        conty->data = oldContyData;
+        return NULL;
+    }
+    conty->size = size;
+    if(copy) {
+        memcpy(conty->data, oldContyData, size);
+    }
+    return conty;
+}
 
 Continuity_t*
 Continuity_nResize(
@@ -38,28 +66,100 @@ Continuity_nResize(
     const size_t        size,
     const size_t        desBeginIndex,
     const size_t        srcBeginIndex,
-    const size_t        copySize
-);
+    const size_t        copySize)
+{
+    if(conty == NULL) {
+        return NULL;
+    }
+    if(size == 0) {
+        Continuity_free(conty);
+        return conty;
+    }
+    if(conty->size == 0) {
+        return Continuity_resize(conty, size, 0);
+    }
+    if(desBeginIndex >= size || srcBeginIndex >= conty->size) {
+        return Continuity_resize(conty, size, 1);
+    }
+
+    void *const oldContyData = conty->data;
+    conty->data = Continuity_malloc(size);
+    if(conty->data == NULL) {
+        conty->data = oldContyData;
+        return NULL;
+    }
+    memcpy((uint8_t*)conty->data + desBeginIndex, (uint8_t*)oldContyData + srcBeginIndex, copySize);
+    conty->size = size;
+    free(oldContyData);
+    return conty;
+}
 
 Continuity_t*
 Continuity_free(
-    Continuity_t *const conty
-);
+    Continuity_t *const conty)
+{
+    if(conty == NULL) {
+        return NULL;
+    }
+    free(conty->data);
+    conty->data = NULL;
+    conty->size = 0;
+    return conty;
+}
 
 Continuity_t*
 Continuity_nCopy(
     Continuity_t *const conty,
     const void *const   data,
     const size_t        size,
-    const size_t        index
-);
+    const size_t        index)
+{
+    if(conty == NULL) {
+        return NULL;
+    }
+    if(data == NULL || index > conty->size) {
+        return NULL;
+    }
+    if(size == 0) {
+        return conty;
+    }
+    if(size + index > conty->size) {
+        Continuity_resize(conty, size + index, 1);
+    }
+    if(conty->data == NULL) {
+        conty->size = 0;
+        return NULL;
+    }
+    memcpy((uint8_t*)conty->data + index, data, size);
+    return conty;
+}
 
 Continuity_t*
 Continuity_copy(
     Continuity_t *const       desConty,
-    const Continuity_t *const srcConty
-);
+    const Continuity_t *const srcConty)
+{
+    if(srcConty == NULL) {
+        return NULL;
+    }
+    if(desConty == NULL) {
+        Continuity_resize(desConty, srcConty->size, 0);
+        if(desConty == NULL) {
+            return NULL;
+        }
+        memcpy(desConty->data, srcConty->data, srcConty->size);
+        return desConty;
+    }
+    if(desConty->size < srcConty->size) {
+        Continuity_resize(desConty, srcConty->size, 0);
+        if(desConty->data == NULL) {
+            return NULL;
+        }
+        memcpy(desConty->data, srcConty->data, srcConty->size);
+        return desConty;
+    }
+    memcpy(desConty->data, srcConty->data, srcConty->size);
+    return desConty;
+}
 
-#include "src/Continuity.c"
-
-#endif /* _CONTINUITY_H_ */
+#endif /* _CONTINUITY_C_ */
